3622785b264712a1cd041a7f12ad4f67befb1fc1
Jack Flannery <hello@jackflannery.com>
2023-11-13 13:51:34 -0500

    Fix puma prod conf

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -7,14 +7,14 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { 1 }
 threads min_threads_count, max_threads_count
 
-app_dir = File.expand_path("../..", __FILE__)
+app_dir = File.expand_path("../../../../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-bind "unix:/#{shared_dir}/sockets/puma.sock"
+bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
