d3136e2951569aa4f60204a8470fbfcfdc4089ec
Jack Flannery <hello@jackflannery.com>
2024-03-05 13:14:07 -0500

    Update capistrano's rbenv_ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 set :branch, :main
 
 set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
-set :rbenv_ruby, '3.2.2'
+set :rbenv_ruby, '3.3.0'
 
 append :linked_files, 'config/database.yml', 'config/credentials/production.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
