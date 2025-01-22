bdbc6947afa0cdfa9b19e0eba1172c7b8d69678e
Jack Flannery <hello@jackflannery.com>
2025-01-22 14:44:35 -0500

    Update cap's ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.18.0"
+lock "~> 3.19.2"
 
 set :application, "my_api"
 set :repo_url, "git@github.com:jmflannery/my_api.git"
@@ -9,7 +9,7 @@ set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 set :branch, :main
 
 set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
-set :rbenv_ruby, '3.3.0'
+set :rbenv_ruby, '3.4.1'
 
 append :linked_files, 'config/credentials/production.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
