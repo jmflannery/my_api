b605106620fb02c0f3264ce7de3619e3ed5bbd3a
Jack Flannery <hello@jackflannery.com>
2025-01-23 10:22:25 -0500

    Set  current and target ruby version for update ruby task

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,11 @@ set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 set :branch, :main
 
 set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
-set :rbenv_ruby, '3.4.1'
+set :rbenv_ruby, '3.3.0'
+set :rbenv_target_ruby, '3.4.1'
+
+# set :default_env, { path: "#{fetch(:rbenv_path)}/bin:$PATH" }
+# set :rbenv_prefix, "#{fetch(:rbenv_path)}/bin/rbenv exec"
 
 append :linked_files, 'config/credentials/production.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
